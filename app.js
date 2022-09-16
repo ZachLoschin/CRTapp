bda61e456702a1e7a37c8d4e6595cc72c12f01c3
ZachLoschin <zachloschin@gmail.com>
2022-09-16 13:06:50 -0500

    Try to work w/ IOS cam

---
+++ app.js
@@ -0,0 +1,43 @@
+// Define global variables 
+
+let VIDEO=null;
+let CANVAS=null;
+let CONTEXT=null;
+
+function main() {
+    // Define canvas with myCanvas which is defined in the HTML
+    CANVAS = document.getElementById("myCanvas");
+    CONTEXT = CANVAS.getContext("2d");
+
+    // Create the canvas size
+    CANVAS.width=window.innerWidth;
+    CANVAS.height=window.innerHeight;
+
+    // Ask for access to video device
+    let promise=navigator.mediaDevices.getUserMedia({video:true});
+
+    // After getting acces to video device
+    promise.then(function(signal){
+        // Attach the video element to the VIDEO and give it the signal
+        VIDEO=document.createElement("video");
+        VIDEO.setAttribute("playsinline", true);
+        VIDEO.srcObject=signal;
+        VIDEO.play();
+
+        // When video data is available, update it on the canvas
+        VIDEO.onloadeddata = function() {
+            updateCanvas();
+        }
+
+    }).catch(function(err){
+        alert("Camera error; "+err)
+    })
+}
+
+// Function to update the canvas
+function updateCanvas() {
+    // Draws the image to the canvas starting at coord(0,0) aka the topleft
+    CONTEXT.drawImage(VIDEO,0,0);
+
+    window.requestAnimationFrame(updateCanvas);
+}
